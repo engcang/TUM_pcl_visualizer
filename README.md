0a92ad0974d0ac59813dcce153c144160993ac65
engcang <eungchang_mason@kaist.ac.kr>
2025-02-01 10:46:34 +0900

    done

--- README.md
+++ README.md
@@ -1,49 +1,47 @@
-<h2 align="center"><span>LODESTAR: A Robust LiDAR-Inertial Odometry<br>Using Degeneracy-Aware Adaptive Schmidt Filter<br>with Sliding Window and Data Exploitation</span></h2>
+<h2 align="center"><span>TUM PCL Visualizer</span></h2>
+
+- Visualizing tool to check quality of the map with the estimated pose of your odometry / SLAM algorithm
+    - It gets `TUM` format estimated pose - tx px py pz qx qy qz qw
+    - It gets the `rosbag` file that used for pose estimation
+    - It publishes the transformed pointcloud to estimated pose frame
+    - It also publishes the estimated odometry in nav_msgs/Path
 
 <br>
 
 ## Dependencies
-+ `C++` >= 17, `CMake` >= 3.0, `Eigen` >= 3.2
++ `C++` `CMake`, `Eigen`
 + `ROS1`
-+ `tbb`
++ `yaml-cpp`
     ```bash
-    sudo apt install libtbb-dev
+    sudo apt install libyaml-cpp-dev
     ```
++ `livox_ros_driver` package for converting livox LiDAR data from many datasets
+    + Specifically, `livox_ros_driver/CustomMsg`
 
 <br>
 
-## How to build
+## How to build and use
 + Get the code and then build
     ```bash
     cd ~/your_workspace/src
-    git clone https://github.com/engcang/Lodestar --recursive
+    git clone https://github.com/engcang/tum_pcl_visualizer
 
     cd ~/your_workspace
     catkin build -DCMAKE_BUILD_TYPE=Release
+    ```
++ Run the code and watch the pointcloud with `Rviz`
+    ```bash
+    rosrun tum_pcl_visualizer tum_pcl_visualizer_node config_file bag_file_abs_path tum_csv_file_abs_path publish_hz max_t_diff_btw_pcl_and_pose time_offset
     ```
 
 <br>
 
 ## TODO
+- [ ] velodyne packets to sensor_msgs::PointCloud2
 - [ ] `ROS2`
 
 <br>
 
 ## LICENSE
-+ This version of repository is based on [FAST-LIO](https://github.com/hku-mars/FAST_LIO) and hence following `GPL-2.0`
-+ `LODESTAR` itself is following the license:
-    ```markdown
-    Copyright (c) 2025, EungChang-Mason-Lee
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, ONLY WITHOUT
-    modification, are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-
-    2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-    3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-    4. Redistributions or use in any forms are NOT FOR ANY METHOD OF COMMERCIAL USE.
-    ```
++ This code is coded by myself and ChatGPT and hence it is free to use, distribute, etc.
++ I guess it is under BSD3 or MIT license.
--- src/main_node.cpp
+++ src/main_node.cpp
@@ -2,13 +2,17 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <nav_msgs/Path.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/transforms.h>
-#include <tf2_eigen/tf2_eigen.h>
+#include <tf/transform_datatypes.h>
+#include <tf_conversions/tf_eigen.h>
 #include <Eigen/Dense>
 #include <fstream>
 #include <sstream>
 #include <map>
+#include <yaml-cpp/yaml.h> // YAML 라이브러리
+#include <livox_ros_driver/CustomMsg.h>
 
 // TUM CSV 데이터를 저장할 구조체
 struct TumPose
@@ -17,6 +21,78 @@ struct TumPose
     Eigen::Vector3d position;
     Eigen::Quaterniond orientation;
 };
+
+// YAML 파일에서 extrinsic_R (3x3 행렬)과 extrinsic_T (3x1 벡터) 읽기
+void loadExtrinsics(const std::string &config_file, Eigen::Matrix3d &extrinsic_R, Eigen::Vector3d &extrinsic_T, std::string &topic_name)
+{
+    YAML::Node config = YAML::LoadFile(config_file);
+
+    if (!config["extrinsic_R"] || !config["extrinsic_R"].IsSequence() || config["extrinsic_R"].size() != 9)
+    {
+        throw std::runtime_error("Invalid extrinsic_R format in YAML file! Expected 9 elements.");
+    }
+
+    for (int i = 0; i < 9; ++i)
+    {
+        extrinsic_R(i / 3, i % 3) = config["extrinsic_R"][i].as<double>();
+    }
+
+    if (!config["extrinsic_T"] || !config["extrinsic_T"].IsSequence() || config["extrinsic_T"].size() != 3)
+    {
+        throw std::runtime_error("Invalid extrinsic_T format in YAML file! Expected 3 elements.");
+    }
+
+    for (int i = 0; i < 3; ++i)
+    {
+        extrinsic_T(i) = config["extrinsic_T"][i].as<double>();
+    }
+
+
+    topic_name = config["topic_name"].as<std::string>();
+
+    // 디버깅용 출력
+    std::cout << "extrinsic_R:" << std::endl;
+    std::cout << extrinsic_R << std::endl;
+    std::cout << "extrinsic_T: " << extrinsic_T.transpose() << std::endl;
+}
+
+// 변환 행렬 생성 함수 (TUM 포즈 → Eigen pose 변환 행렬)
+Eigen::Matrix4d getTransformMatrix(const TumPose &pose, const Eigen::Matrix4d &extrinsic_transform)
+{
+    Eigen::Matrix4d pose_eig_out = Eigen::Matrix4d::Identity();
+
+    // 1. 쿼터니언을 TF로 변환
+    tf::Quaternion quat(pose.orientation.x(), pose.orientation.y(), pose.orientation.z(), pose.orientation.w());
+
+    // 2. TF 행렬을 Eigen 행렬로 변환
+    tf::Matrix3x3 tf_mat(quat);
+    Eigen::Matrix3d tmp_rot_mat;
+    tf::matrixTFToEigen(tf_mat, tmp_rot_mat);
+
+    // 3. 회전 행렬 적용 (기존 회전에 extrinsic_R 추가 적용)
+    pose_eig_out.block<3, 3>(0, 0) = tmp_rot_mat;
+
+    // 4. 위치 변환 적용 (기존 위치에 extrinsic_R, extrinsic_T 적용)
+    pose_eig_out.block<3, 1>(0, 3) = pose.position;
+
+    // 5. Extrinsics 변환 행렬 적용
+
+    return pose_eig_out * extrinsic_transform;
+}
+
+// PCL 다운샘플링 함수
+pcl::PointCloud<pcl::PointXYZI> downsamplePointCloud(const pcl::PointCloud<pcl::PointXYZI> &cloud, int filter_num)
+{
+    pcl::PointCloud<pcl::PointXYZI> filtered_cloud;
+    filtered_cloud.reserve(cloud.size() / filter_num);
+
+    for (size_t i = 0; i < cloud.size(); i += filter_num)
+    {
+        filtered_cloud.push_back(cloud[i]);
+    }
+
+    return filtered_cloud;
+}
 
 // TUM CSV를 읽어서 map에 저장
 std::map<double, TumPose> loadTumCsv(const std::string &filename)
@@ -70,24 +146,52 @@ bool findClosestPose(double timestamp, double offset, double max_time_diff, cons
     return true;
 }
 
+// Livox CustomMsg를 PCL 포인트 클라우드로 변환
+pcl::PointCloud<pcl::PointXYZI> convertLivoxToPCL(const livox_ros_driver::CustomMsg::ConstPtr &msg)
+{
+    pcl::PointCloud<pcl::PointXYZI> pcl_cloud;
+    size_t num_points = msg->point_num;
+    pcl_cloud.reserve(num_points);
+
+    for (size_t i = 1; i < num_points; i++)
+    {
+        if ((msg->points[i].tag & 0x30) == 0x10 || (msg->points[i].tag & 0x30) == 0x00)
+        {
+            pcl::PointXYZI point;
+            point.x = msg->points[i].x;
+            point.y = msg->points[i].y;
+            point.z = msg->points[i].z;
+            point.intensity = msg->points[i].reflectivity;
+            pcl_cloud.push_back(point);
+        }
+    }
+    return pcl_cloud;
+}
+
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "bag_pcl_transformer");
     ros::NodeHandle nh;
-    ros::Publisher pub_pcl = nh.advertise<sensor_msgs::PointCloud2>("/transformed_pcl", 1);
+    ros::Publisher pub_pcl = nh.advertise<sensor_msgs::PointCloud2>("/tum_pcl_visualize", 1);
+    ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/tum_path_visualize", 1);
+    nav_msgs::Path tum_path;
+    tum_path.header.frame_id = "map";
 
-    // 명령줄 인자 받기: bag_file, csv_file, topic_name, time_offset
-    if (argc < 5)
+    // 명령줄 인자 받기
+    if (argc != 8)
     {
-        ROS_ERROR("Usage: rosrun package bag_pcl_transformer bag_file csv_file topic_name time_offset");
+        ROS_ERROR("Usage: rosrun package bag_pcl_transformer config_file bag_file csv_file publish_hz point_filter_num max_time_diff time_offset");
         return -1;
     }
 
-    std::string bag_file = argv[1];
-    std::string tum_csv = argv[2];
-    std::string pcl_topic = argv[3];
-    double time_offset = std::stod(argv[4]);
-    double max_time_diff = 0.05; // 최대 허용 시간 차이 (예: 50ms)
+    std::string config_file = argv[1];
+    std::string bag_file = argv[2];
+    std::string tum_csv = argv[3];
+    double publish_hz = std::stod(argv[4]);
+    int point_filter_num = std::stoi(argv[5]);
+    double max_time_diff = std::stod(argv[6]);
+    double time_offset = std::stod(argv[7]);
 
     // TUM CSV 데이터 로드
     auto tum_data = loadTumCsv(tum_csv);
@@ -97,46 +201,106 @@ int main(int argc, char **argv)
         return -1;
     }
 
+    if (point_filter_num < 1)
+    {
+        ROS_ERROR("Invalid point_filter_num! It must be >= 1");
+        return -1;
+    }
+
+    // Config 파일에서 extrinsic_R, extrinsic_T, Topic name 로드
+    Eigen::Matrix3d extrinsic_R = Eigen::Matrix3d::Identity();
+    Eigen::Vector3d extrinsic_T = Eigen::Vector3d::Zero();
+    std::string pcl_topic;
+    loadExtrinsics(config_file, extrinsic_R, extrinsic_T, pcl_topic);
+    Eigen::Matrix4d extrinsic_transform = Eigen::Matrix4d::Identity();
+    extrinsic_transform.block<3, 3>(0, 0) = extrinsic_R;
+    extrinsic_transform.block<3, 1>(0, 3) = extrinsic_T;
+
     // ROSBag 읽기
     rosbag::Bag bag;
     bag.open(bag_file, rosbag::bagmode::Read);
     rosbag::View view(bag, rosbag::TopicQuery(pcl_topic));
 
+    // 메시지 타입 확인
+    std::string topic_type;
     for (const auto &m : view)
     {
-        sensor_msgs::PointCloud2::ConstPtr cloud_msg = m.instantiate<sensor_msgs::PointCloud2>();
-        if (!cloud_msg)
-            continue;
+        topic_type = m.getDataType();
+        break;
+    }
+    ROS_INFO("Detected topic type: %s", topic_type.c_str());
 
-        double msg_time = cloud_msg->header.stamp.toSec();
+    for (const auto &m : view)
+    {
+        sensor_msgs::PointCloud2 transformed_msg;
+        pcl::PointCloud<pcl::PointXYZI> pcl_cloud;
+
+        // 메시지 타입에 따라 처리
+        if (topic_type == "sensor_msgs/PointCloud2")
+        {
+            sensor_msgs::PointCloud2::ConstPtr cloud_msg = m.instantiate<sensor_msgs::PointCloud2>();
+            if (!cloud_msg)
+                continue;
+
+            pcl::fromROSMsg(*cloud_msg, pcl_cloud);
+            transformed_msg.header.stamp = cloud_msg->header.stamp;
+        }
+        else if (topic_type == "livox_ros_driver/CustomMsg")
+        {
+            livox_ros_driver::CustomMsg::ConstPtr livox_msg = m.instantiate<livox_ros_driver::CustomMsg>();
+            if (!livox_msg)
+                continue;
+
+            pcl_cloud = convertLivoxToPCL(livox_msg);
+            transformed_msg.header.stamp = ros::Time(livox_msg->timebase * 1e-9); // timestamp 변환
+        }
+        else
+        {
+            ROS_ERROR("Unsupported topic type: %s", topic_type.c_str());
+            break;
+        }
+
+        double msg_time = transformed_msg.header.stamp.toSec();
         TumPose closest_pose;
 
         // offset 및 최대 허용 시간 차이를 반영하여 timestamp 찾기
         if (!findClosestPose(msg_time, time_offset, max_time_diff, tum_data, closest_pose))
         {
             ROS_WARN("Skipping pointcloud at %.6f: No matching TUM pose found", msg_time);
-            continue; // 최대 시간 차이를 넘으면 publish하지 않음
+            continue;
         }
 
-        // 변환 행렬 생성 (TUM 좌표 -> world 좌표)
-        Eigen::Affine3d transform = Eigen::Affine3d::Identity();
-        transform.translation() = closest_pose.position;
-        transform.linear() = closest_pose.orientation.toRotationMatrix();
+        // TF 변환을 사용하여 변환 행렬 생성
+        Eigen::Matrix4d transform_matrix = getTransformMatrix(closest_pose, extrinsic_transform);
 
         // PCL 변환
-        pcl::PointCloud<pcl::PointXYZ> pcl_cloud, transformed_cloud;
-        pcl::fromROSMsg(*cloud_msg, pcl_cloud);
-        pcl::transformPointCloud(pcl_cloud, transformed_cloud, transform);
+        pcl::PointCloud<pcl::PointXYZI> transformed_cloud;
+        pcl::transformPointCloud(pcl_cloud, transformed_cloud, transform_matrix);
+        pcl::PointCloud<pcl::PointXYZI> filtered_cloud = downsamplePointCloud(transformed_cloud, point_filter_num);
 
         // 변환된 PointCloud Publish
-        sensor_msgs::PointCloud2 transformed_msg;
-        pcl::toROSMsg(transformed_cloud, transformed_msg);
-        transformed_msg.header.frame_id = "world";
-        transformed_msg.header.stamp = cloud_msg->header.stamp;
+        pcl::toROSMsg(filtered_cloud, transformed_msg);
+        transformed_msg.header.frame_id = "map";
         pub_pcl.publish(transformed_msg);
 
-        // 1ms sleep
-        ros::Duration(0.001).sleep();
+        // TUM Path Publish
+        geometry_msgs::PoseStamped pose_stamped;
+        pose_stamped.header.stamp = transformed_msg.header.stamp;
+        pose_stamped.header.frame_id = "map";
+        pose_stamped.pose.position.x = closest_pose.position.x();
+        pose_stamped.pose.position.y = closest_pose.position.y();
+        pose_stamped.pose.position.z = closest_pose.position.z();
+        pose_stamped.pose.orientation.x = closest_pose.orientation.x();
+        pose_stamped.pose.orientation.y = closest_pose.orientation.y();
+        pose_stamped.pose.orientation.z = closest_pose.orientation.z();
+        pose_stamped.pose.orientation.w = closest_pose.orientation.w();
+
+        tum_path.poses.push_back(pose_stamped);
+        tum_path.header.stamp = pose_stamped.header.stamp;
+        path_pub.publish(tum_path);
+
+        // sleep
+        ros::Duration(1.0 / publish_hz).sleep();
     }
 
     bag.close();
